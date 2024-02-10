01d4428ba1cb633df3645a3e721b16a533b2ddf8
pashKacapiBaraKodland <156570934+pashKacapiBaraKodland@users.noreply.github.com>
2024-02-10 17:11:25 +1100

    Update spamd.py

--- spamd.py
+++ spamd.py
@@ -1,10 +1,10 @@
 import random 
 
 
-def spamm(long):
+def spamm():
     char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     spamh = ''
-    long = random.randint(5, 6)
+    long = random.randint(5, 60)
     for i in range(long):
         spamh += random.choice(char)
     return spamh
